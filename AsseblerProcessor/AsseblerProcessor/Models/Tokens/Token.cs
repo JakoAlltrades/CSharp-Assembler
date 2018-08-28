1c70a87a604abcd1aa5016a34fd66f9b9a8e06a6
JakoAlltrades <Jpriemo@gmail.com>
2018-08-27 22:31:31 -0600

    Initial Commit
    A start to the assembly processor due on thursday to graduate.

---
+++ AsseblerProcessor/AsseblerProcessor/Models/Tokens/Token.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsseblerProcessor.Models.Tokens
+{
+    public abstract class Token
+    {
+        public abstract bool Process(string cond);
+    }
+}
